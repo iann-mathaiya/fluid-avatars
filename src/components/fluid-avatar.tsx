9c0b30f9de91ca1ce8886fa94046a68cfeac01e0
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-22 16:04:39 +0300

    update fluid avatar

--- src/components/fluid-avatar.tsx
+++ src/components/fluid-avatar.tsx
@@ -1,16 +1,28 @@
 import { MeshGradient } from '@paper-design/shaders-react';
+import { useState } from 'react';
+
+const defaultColorPalette = ['#FFB238', '#FF7D10', '#FF005B', '#49007E'];
 
 export default function FluidAvatar() {
+    const [colorOne, setColorOne] = useState(defaultColorPalette[0]);
+    const [colorTwo, setColorTwo] = useState(defaultColorPalette[1]);
+    const [colorThree, setColorThree] = useState(defaultColorPalette[2]);
+    const [colorFour, setColorFour] = useState(defaultColorPalette[3]);
+
+    function generateNewPalette(){
+        //TO DO
+    }
+
     return (
-        <div className="min-w-28 size-28 sm:min-w-20 sm:size-20 overflow-hidden rounded-2xl">
+        <button type='button' onClick={generateNewPalette} className="min-w-28 size-28 sm:min-w-20 sm:size-20 overflow-hidden rounded-2xl">
             <MeshGradient
-                color1="#FFB238"
-                color2="#FF7D10"
-                color3="#FF005B"
-                color4="#49007E"
+                color1={colorOne}
+                color2={colorTwo}
+                color3={colorThree}
+                color4={colorFour}
                 speed={0.25}
                 style={{ width: '100%', height: '100%' }}
             />
-        </div>
+        </button>
     );
 }
