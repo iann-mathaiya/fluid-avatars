2373d4b1b552cb15131e4f8bcb75fcb3676a6ae6
Ian Mathaiya <iannmathaiya@gmail.com>
2025-03-22 15:04:05 +0300

    add react and mesh gradient in header

---
+++ src/components/fluid-avatar.tsx
@@ -0,0 +1,16 @@
+import { MeshGradient } from '@paper-design/shaders-react';
+
+export default function FluidAvatar() {
+    return (
+        <div className="min-w-28 size-28 sm:min-w-20 sm:size-20 overflow-hidden rounded-2xl">
+            <MeshGradient
+            color1="#FFB238"
+            color2="#FF7D10"
+            color3="#FF005B"
+            color4="#49007E"
+            speed={0.25}
+            style={{width: '100%', height: '100%' }}
+        />
+        </div>
+    )
+}
